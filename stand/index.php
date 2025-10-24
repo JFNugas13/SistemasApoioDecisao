b6a15249ad5e572df9046fb342cf543b295fae6c
Kezys <156124387+kezys@users.noreply.github.com>
2025-10-24 16:22:06 +0100

    Add initial project files including database connection and styles

---
+++ stand/index.php
@@ -0,0 +1,31 @@
+<?php
+// stand/index.php
+include("includes/db_conn.php");
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
+<body>
+    <header>
+        <h1>Bem-vindo ao Stand de Automóveis</h1>
+        <nav>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="#">Catálogo de Automóveis</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
